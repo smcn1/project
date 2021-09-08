dd161ca0b62d02a9b5458404d3493e1c627d64a8
smcn1 <stevenmcnally@live.co.uk>
2021-09-08 21:45:50 +0100

    Update lotto.js

--- js/lotto.js
+++ js/lotto.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('lotto_entry')
 
-// const name = document.getElementById('name')
-// const address = document.getElementById('address')
-// const phone = document.getElementById('phone')
+const name = document.getElementById('name')
+const address = document.getElementById('address')
+const phone = document.getElementById('phone')
 const num1 = document.getElementById('num1')
 const num2 = document.getElementById('num2')
 const num3 = document.getElementById('num3')
@@ -12,6 +12,18 @@ const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = []
+    if (name.value === '' || name.value == null) {
+        messages.push('Name required')
+    }
+    
+    if (address.value === '' || address.value == null) {
+        messages.push('Address required')
+    }
+    
+    if (phone.value === '' || phone.value == null) {
+        messages.push('Phone required')
+    }
+    
     if (num1.value == num2.value || num1.value == num3.value || num1.value == num4.value || num2.value == num3.value || num2.value == num4.value || num3.value == num4.value) {
         messages.push('Pick 4 different numbers')
     }
